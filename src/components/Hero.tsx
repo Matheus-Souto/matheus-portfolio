74ebd78a4ba52bb60a395ee818691c7feb8507c2
Souto <matheus.souto@marinha.mil.br>
2023-08-21 13:54:56 -0300

    feat: partial hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+export const Hero = () => {
+    return(
+        <section className="flex py-44 container max-w-[1000px] mx-auto items-center">
+            <div className="max-w-lg relative">
+                <h1 className="font-bold text-5xl text-slate-800 leading-snug">Front-End React Developer</h1>
+                <img className="h-14 absolute top-[65px] left-[300px]" src="/waving.png" alt="" />
+                <p className="mt-3 text-gray-600 text-lg">Hi, I'm Stefan Topalovic. A passionate Front-end React Developer based in Belgrade, Serbia. 📍</p>
+            </div>
+            <div>
+
+            </div>
+        </section>
+    );
+}
